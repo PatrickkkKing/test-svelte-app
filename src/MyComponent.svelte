fee28a66d26537b4814cc020843a13598d400932
Putra, M.A <muklasputra222@gmail.com>
2024-09-29 04:09:40 +0700

    first commit

---
+++ src/MyComponent.svelte
@@ -0,0 +1,14 @@
+<script>
+    let name = 'Putra';
+    import { count } from './store.js';
+
+    function increment() {
+        count.update(n => n + 1);
+    }
+</script>
+
+<h2>Selamat datang di Svelte, {name}!</h2>
+<p>Ini adalah komponen Svelte sederhana.</p>
+
+<h2>Jumlah: {$count}</h2>
+<button on:click={increment}>Tambah</button>
